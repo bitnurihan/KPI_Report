a9a7dce9205e540c86e8ae2dfdf721b707c6e19e
Nuri <gksqlcsnfl@gmail.com>
2019-03-14 10:10:47 +0900

    date_cell update

--- SBSMonthly2.py
+++ SBSMonthly2.py
@@ -4,9 +4,8 @@ from copy import copy
 import xlrd
 
 
-
 def read_data_from_excel(write_file_name):
-    global read_excel_file, worksheet_read, month, row, col, array, inner_array, i, j, value
+    global read_excel_file, worksheet_read, current_month, row, col, array, inner_array, i, j, value
     read_excel_file = xlrd.open_workbook(write_file_name)
     worksheet_read = read_excel_file.sheet_by_name('전체 수도권 (P) - 가구 (1408)')
     row = 3
@@ -36,52 +35,77 @@ def paste_to_excel(row, col):
         print()
 
 
+def get_work_line():
+    start_line = 376
+    init_year = 2019  # 19년 1월 378라인
+    current_year = datetime.now().year
+    current_month = datetime.now().month
+    extra_line = 0
+    if current_month > 3:
+        extra_line = 2
+    elif current_month > 6:
+        extra_line = 4
+    elif current_month > 9:
+        extra_line = 6
+
+    return start_line + (current_year - init_year) * 34 + (current_month - 1) * 2 + extra_line
+
+
+def hidden_cells():
+    global col
+    for col in ['N', 'O', 'P']:
+        worksheet_write.column_dimensions[col].hidden = True
+
+
+def get_zero_month():
+    month = (datetime.now().month - 1)
+
+    if month < 10:
+        month = "0" + str(month)
+
+    return str(month)
+
+
 ###1. 기존전시간대시청률(06-11,17-24)
 write_excel_file = load_workbook(filename=r'C:\Users\hanbi01\Desktop\한빛누리\(매월)SBS월간업데이트\MonthlyReport2.xlsx')
 
 read_data_from_excel(r'C:\Users\hanbi01\Desktop\한빛누리\(매월)SBS월간업데이트\1.xls')
 worksheet_write = write_excel_file[r'기존전시간대시청률(06-11,17-24)']
 
-paste_to_excel(380, 3)  ## 380이 매달 +2씩 증가할 수 있게 수정해야 함.
-
+paste_to_excel(get_work_line(), 3)
 
 ### 셀 스타일 복사해서 붙여넣기
 
-row = 378 ## 378이 매달 +2씩 증가할 수 있게 수정해야 함.
+row = get_work_line()
 col = 1
 
 for i in range(2):
     for j in range(16):
-        worksheet_write.cell(row+2, col)._style = copy(worksheet_write.cell(row, col)._style)
+        worksheet_write.cell(row, col)._style = copy(worksheet_write.cell(row - 2, col)._style)
         col += 1
 
     row += 1
     col = 1
 
-## 함수 복붙하기
+work_line = get_work_line()
 
-
-row = 378 ## 378이 매달 +2씩 증가할 수 있게 수정해야 함.
+row = get_work_line()
 col = 1
 
 for i in range(1):
     for j in range(16):
-        worksheet_write.cell(row+3, col).value = copy(worksheet_write.cell(row+1, col).value) # 이렇게 했더니 379번줄 그대로 가져와서 문제생김.. -_-
+        worksheet_write.cell(row + 1, col).value = copy(
+            worksheet_write.cell(row - 1, col).value)  # 이렇게 했더니 379번줄 그대로 가져와서 문제생김.. -_-
         col += 1
 
+hidden_cells() #셀숨기기
 
-### 셀숨기기
-for col in ['N', 'O', 'P']:
-    worksheet_write.column_dimensions[col].hidden = True
+(worksheet_write.cell(row=get_work_line(), column=2)).value = "Viewership"
 
-## 날짜넣기
 
-row = 380 ## 380이 매달 +2씩 증가할 수 있게 수정해야 함.
-month = datetime.now().month
+date_cell = worksheet_write.cell(row=(get_work_line()), column=1) ## 날짜넣기
 
-year = datetime.now().year
-date_cell = worksheet_write.cell(row=row, column=1)
-date_cell.value = "%s. %s" % (year, (month-1))
 
+date_cell.value = "{}.{}".format(datetime.now().year, get_zero_month())
 
 write_excel_file.save('testfile2.xlsx')

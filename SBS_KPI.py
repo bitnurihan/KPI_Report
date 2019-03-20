d648126a378ad1a9f9426a4b6f5c5a3244c74eaf
Nuri <gksqlcsnfl@gmail.com>
2019-03-20 10:46:35 +0900

    ㅜㅜ..

--- SBS_KPI.py
+++ SBS_KPI.py
@@ -4,10 +4,6 @@ import data as data
 import pandas as pd
 import xlrd
 
-path_dir = r'C:\Users\hanbi01\Desktop\한빛누리\(분기)KPI\raw_data'
-
-file_list = os.listdir(path_dir)
-
 # Nuri = {"program_name" :  '그것이 알고싶다', 'copy_cells': [1, 2, 3, 4, 5]}
 # print(Nuri['program_name'])
 
@@ -18,10 +14,11 @@ def delete_cols():
         worksheet_read.column_dimensions[col].hidden = True
 
 
+path_dir = r'C:\Users\hanbi01\Desktop\한빛누리\(분기)KPI\raw_data'
+file_list = os.listdir(path_dir)
+
 for file in file_list:
     raw_data_files = xlrd.open_workbook('%s\%s' % (path_dir, file))
 
     print(file)
 
-worksheet_read = raw_data_files.sheet_by_name('sbs_kpi_')
-delete_cols()
